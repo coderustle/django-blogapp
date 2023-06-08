49217423e4c79cc46e9a921b8738e10e2237d81a
Madalin Popa <contact@madalinpopa.com>
2023-06-08 20:21:33 +0300

    Add django filters

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
+from django.db.models import Count
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post
@@ -25,6 +26,7 @@ def post_list(request, tag_slug=None):
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
@@ -59,10 +61,22 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
